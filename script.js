63005ec9f7aaa0be3c88f02a1bcbb4b3cdc4cfe5
ThiagoMachadoL <thiagojkp123@gmail.com>
2022-07-28 17:40:31 -0300

    aula 10 - Mazzi

---
+++ script.js
@@ -0,0 +1,18 @@
+// Array first
+const strings = ['caneta','régua','garrafa']
+// Array Second
+const numeros = [27,35,42]
+// Array Third
+const misto = ['celular', 5, true]
+// Array Fourth
+const numberUnico = [1996]
+// Array Fifth
+const arrayVazio = []
+
+console.log('TAMANHO ARRAY STRINGS', strings.length)
+console.log('ACESSANDO ITENS ARRAYS',strings[2])
+console.log('VERIFICANDO ITENS DO ARRAY 3', misto.includes('celular'))
+console.log('VERIFICANDO ITENS DO ARRAY 3', misto.includes('5'))
+console.log('VERIFICANDO ITENS DO ARRAY 3', misto.includes('true'))
+console.log('VERIFICANDO ITEM DO ARRAY 4', numberUnico[0])
+console.log('VERIFICANDO TAMANHO DO ARRAY 4', numberUnico.length)
